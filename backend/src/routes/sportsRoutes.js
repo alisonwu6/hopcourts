e5438f3362ad220dbcaea659f8130d5eb92c3fc0
Alison Wu <alison.wu23@gmail.com>
2025-12-20 11:46:40 +1000

    chore(backend): remove legacy routes, controllers, and models

--- backend/src/routes/sportsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { listSports } = require('../controllers/sportsController')
-
-const router = express.Router()
-
-router.get('/sports', listSports)
-
-module.exports = router
--- backend/src/routes/venueRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { handleGetVenue, handleListVenues } = require('../controllers/venueController')
-
-router.get('/', handleListVenues)
-router.get('/:id', handleGetVenue)
-
-module.exports = router
