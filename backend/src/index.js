fc8c46c27b55ca6d38018b016e3374c6716f839c
Alison Wu <alison.wu23@gmail.com>
2026-02-06 14:33:06 +0800

    chore: Update default server port from 3000 to 8080.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const { createApp } = require('./app')
 
 const app = createApp()
 
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 8080;
 app.listen(port, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log(`API listening on http://0.0.0.0:${port}${process.env.API_BASE_PATH || ""}`);
