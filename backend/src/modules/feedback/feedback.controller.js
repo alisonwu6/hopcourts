1b1932d4385240835c44ba96c5f6c7cdeca67989
Alison Wu <alison.wu23@gmail.com>
2026-07-04 17:54:21 +1000

    refactor: streamline feedback controller response handling and formatting

--- backend/src/modules/feedback/feedback.controller.js
+++ backend/src/modules/feedback/feedback.controller.js
@@ -1,9 +1,10 @@
-const feedbackService = require('./feedback.service');
+const { ok } = require('../../lib/respond')
+const feedbackService = require('./feedback.service')
 
 exports.createFeedback = async (req, res, next) => {
   try {
-    const { type, message, allow_reply, images, guest_email } = req.body;
-    const user_id = req.user ? req.user.id : null;
+    const { type, message, allow_reply, images, guest_email } = req.body
+    const user_id = req.user ? req.user.id : null
 
     const result = await feedbackService.createFeedback({
       user_id,
@@ -12,10 +13,10 @@ exports.createFeedback = async (req, res, next) => {
       allow_reply,
       images,
       guest_email: user_id ? null : (guest_email || null),
-    });
+    })
 
-    res.status(201).json({ success: true, id: result.id });
+    return ok(res, { id: result.id }, 201)
   } catch (err) {
-    next(err);
+    next(err)
   }
-};
+}
